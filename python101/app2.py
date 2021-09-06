36a2330018624f75272635690ebc4ccf1397cd51
sisiishereb <set38246@marymount.edu>
2021-09-06 16:33:43 -0400

    updating one of the old apps and adding yet another new app

---
+++ python101/app2.py
@@ -0,0 +1,8 @@
+birth_year = input("Enter your birth year: ")
+age = 2021 - int(birth_year)
+print(age)
+
+first_number = input("First: ")
+second_number = input("Second: ")
+product = int(first_number) * int(second_number)
+print(product)
