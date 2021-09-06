660819ca535f34d19685de9578a836ac9eb97710
sisiishereb <set38246@marymount.edu>
2021-09-06 16:44:59 -0400

    editing the recently old add and adding yet another new app

---
+++ python101/app3.py
@@ -0,0 +1,7 @@
+course = 'Python for Beginners'
+print(course.upper())
+print(course.lower())
+print(course.find('n'))
+print(course.find('for'))
+print(course.replace('Beginners', 'Newcomers'))
+print('Python' in course)
