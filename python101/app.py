5ff9437293ce26ed6aecdad0820b3d7aec580689
sisiishereb <set38246@marymount.edu>
2021-09-06 16:13:35 -0400

    adding a new app and making changes in the old app

--- python101/app.py
+++ python101/app.py
@@ -1,2 +1,8 @@
+first_name = "Danni"
+last_name = "Andrade"
 age = 20
-print(age)
+hometown = "London, UK"
+is_online = False
+print(first_name, last_name, age, hometown, is_online)
+
+
---
+++ python101/app1.py
@@ -0,0 +1,4 @@
+input("What is your first name?")
+input("What is your last name?")
+input("How old are you?")
+input("Where are you from?")
