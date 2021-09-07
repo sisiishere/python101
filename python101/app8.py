6a71e6cd1d9855dc224c616657c6fb9111b907f6
sisiishereb <set38246@marymount.edu>
2021-09-06 20:36:47 -0400

    editing recent app and adding new app

---
+++ python101/app8.py
@@ -0,0 +1,10 @@
+temperature = 60
+
+if temperature >= 45 and temperature <= 85:
+    print("There is no worry about the cold nor the heat. Yet, bring a light jacket and some water.")
+
+elif temperature < 45:
+    print("It's frigid out, please bring a heavier jacket with a hat, a scarf, and some gloves out with you.")
+
+else:
+    print("It's hot out, please bring some shades, a sunscreen lotion, and a water bottle with you.")
